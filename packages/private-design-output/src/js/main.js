610fce826c07147ae5f10c1ee2f3abdb54166ff1
Nate Millington <n.millington@uq.edu.au>
2020-11-13 14:03:18 +1000

    WIP: prep header for homepage use

--- packages/private-design-output/src/js/main.js
+++ packages/private-design-output/src/js/main.js
@@ -1,11 +1,7 @@
 import header from '@uqds/header/src/js/header.es6';
 import siteHeaderNavigation from '@uqds/site-header/src/js/site-header-navigation.es6';
-import accordion from '@uqds/accordion/src/js/accordion.es6';
-import gridMenuEqualiser from '@uqds/grid-menu/src/js/grid-menu';
 
 export { 
   header,
-  siteHeaderNavigation,
-  accordion,
-  gridMenuEqualiser
+  siteHeaderNavigation
 };
